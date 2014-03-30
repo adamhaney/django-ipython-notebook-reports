e73a1b28451ecc03dd82d1140cd5ae84f5234dff
Adam Haney <adam.haney@akimbo.io>
2014-03-30 18:13:42 -0400

    Roughing the report app schema in a bit more

--- ipython_notebook_reports/models.py
+++ ipython_notebook_reports/models.py
@@ -1,25 +1,38 @@
 from django.db import models
 
 
-class NotebookReport(models.Model):
+class Notebook(models.Model):
     notebook = models.FileField(upload_to='ipython_notebooks')
 
 
-class NotebookReportee(models.Model):
+class ReportManager(models.Manager):
+    def send_reports(self):
+        # Get reports that should be sent now, that haven't already been sent this hour
+        reports = self.filter(day_of_week=now().day, hour=now().hour)
+        reports = reports.exclude(sent_reports__timestamp__hour=now().hour)
+
+        for report in reports:
+            report.notebook.evaluate()
+            report.send()
+
+
+class Report(models.Model):
     """
     Someone who's interested in receiving a report on a regular basis
     """
-    report = models.ForeignKey(NotebookReport)
+    notebook = models.ForeignKey(Notebook)
     email = models.EmailField(
         help_text="The email address of someone who wants a report sent to them"
     )
-
-
-class NoteBookReportTimes(models.Model):
-    """
-    A time when a report for this notebook should be generated and sent
-    """
-    report = models.ForeignKey(NotebookReport)
-    send_time = models.DateTimeField(
-        help_text="A time when the report should be sent, respsects the project timezone setting"
+    day_of_week = models.PositiveIntegerField(
+        help_text="The day of the week when this report should be sent"
     )
+    hour = models.PositiveIntegerField(
+        help_text="The hour of the day in UTC when this report should be sent"
+    )
+
+
+class SentReport(models.Model):
+    report = models.ForeignKey(Report, related_name='sent_reports')
+    output = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
